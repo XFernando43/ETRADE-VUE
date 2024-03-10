3f2e5c6c90fcc8a0175237f1346796c7392fe246
unknown <U20171E997@upc.edu.pe>
2024-03-11 00:09:03 +0100

    header with primeflex

--- src/components/commons/Topbar.vue
+++ src/components/commons/Topbar.vue
@@ -1,32 +1,19 @@
 <template>
-    <header class="header">
-        <img class="logo" src="../../assets/images/logo/logo.png" alt="">
+    <header class="flex flex-row gap-1 align-items-center justify-content-center">
+        <img class="h-full bg-cover" src="../../assets/images/logo/logo.png" alt="">       
         <IconField iconPosition="left" class="iconField-width">
             <InputIcon class="pi pi-search"> </InputIcon>
-            <InputText placeholder="What are you looking for..." class="iconField-width" />
+            <InputText placeholder="What are you looking for..." class="max-w-29rem" />
         </IconField>        
-        <div class="btn-container">
+        <div class="flex flex-row gap-3">
             <Dropdown v-model="selectedLanguage" :options="lenguages" placeholder="Select Language" class="w-full md:w-14rem" />
             <Dropdown v-model="selectedMoney" :options="denominations" placeholder="Select Money" class="w-full md:w-14rem" />
         </div>
     </header>
-
-
-    <div class="flex flex-column">
-        <RouterLink to="/about">about</RouterLink>
-
-        <RouterLink to="/product">product</RouterLink>
-
-    </div>
-
 </template>
 
 <script lang="ts">
-
     import { RouterLink } from 'vue-router';
-
-    import { ref } from 'vue';
-    const value1 = ref(null);
     export default {
         data() {
             return {
@@ -46,31 +33,5 @@
 </script>
 
 <style>
-
-    .iconField-width{
-        width: 900px;
-    }
-
-    .header {
-        display: flex;
-        flex-direction: row;
-        gap: 1rem;
-        max-width: 1440px;
-        align-items: center;
-        margin-inline: auto;
-        padding: 10px;
-    }
-
-    .logo{ 
-        height: 100%;
-        object-fit: cover;
-    }
-
-    .btn-container {
-        display: flex;
-        flex-direction: row;
-        padding: 0.4rem;
-        gap: 0.4rem;
-    }
     
 </style>
