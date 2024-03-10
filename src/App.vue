9c537cc575c32312d8cf1ae0a150ece209bb351b
unknown <U20171E997@upc.edu.pe>
2024-03-10 22:46:13 +0100

    add dependencies

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+
+  <Button label="Submit" />
+
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,42 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/primevue.min.css'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css';
+
+//components
+import Button from 'primevue/button';
+import Dropdown from 'primevue/dropdown';
+import CascadeSelect from 'primevue/cascadeselect';
+import Carousel from 'primevue/carousel';
+import Tag from 'primevue/tag';
+import Image from 'primevue/image';
+import Badge from 'primevue/badge';
+import BadgeDirective from 'primevue/badgedirective';
+import InputText from 'primevue/inputtext';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+
+app.component('Button', Button);
+app.component('Dropdown', Dropdown);
+app.component('IconField', IconField);
+app.component('InputIcon', InputIcon);
+app.component('InputText', InputText);
+
+app.component('CascadeSelect', CascadeSelect);
+app.component('Carousel', Carousel);
+app.component('Tag', Tag);
+app.component('Image', Image);
+
+app.component('Badge', Badge);
+app.directive('badge', BadgeDirective);
+
+app.mount('#app');
