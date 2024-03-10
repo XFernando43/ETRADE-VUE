7a3a2cd454b9a85977bf2ae7cf43196915f78076
unknown <U20171E997@upc.edu.pe>
2024-03-11 00:26:02 +0100

    elimiting css

--- src/components/commons/Topbar.vue
+++ src/components/commons/Topbar.vue
@@ -1,19 +1,23 @@
 <template>
-    <header class="flex flex-row gap-1 align-items-center justify-content-center">
-        <img class="h-full bg-cover" src="../../assets/images/logo/logo.png" alt="">       
-        <IconField iconPosition="left" class="iconField-width">
-            <InputIcon class="pi pi-search"> </InputIcon>
-            <InputText placeholder="What are you looking for..." class="max-w-29rem" />
-        </IconField>        
-        <div class="flex flex-row gap-3">
-            <Dropdown v-model="selectedLanguage" :options="lenguages" placeholder="Select Language" class="w-full md:w-14rem" />
-            <Dropdown v-model="selectedMoney" :options="denominations" placeholder="Select Money" class="w-full md:w-14rem" />
+    <header class="flex flex-row gap-1 align-items-center justify-content-center gap-3">
+        <button class="flex cursor-pointer border-none bg-transparent align-items-center">
+            <img class="h-full bg-cover" src="../../assets/images/logo/logo.png" alt="">       
+        </button>
+
+        <div class="flex flex-row w-30rem gap-2">
+            <IconField iconPosition="left" class="iconField-width">
+                <InputIcon class="pi pi-search"> </InputIcon>
+                <InputText placeholder="What are you looking for..." class="" />
+            </IconField>        
+            <div class="flex flex-row gap-2">
+                <Dropdown v-model="selectedLanguage" :options="lenguages" placeholder="Select Language" class="" />
+                <Dropdown v-model="selectedMoney" :options="denominations" placeholder="Select Money" class="" />
+            </div>
         </div>
     </header>
 </template>
 
 <script lang="ts">
-    import { RouterLink } from 'vue-router';
     export default {
         data() {
             return {
@@ -33,5 +37,5 @@
 </script>
 
 <style>
-    
+   
 </style>
