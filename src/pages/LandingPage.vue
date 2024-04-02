329f925d86eb4003150669187e2ac2608be38b81
XFernando43 <U20171E997@upc.edu.pe>
2024-04-02 16:36:15 -0500

    add: main section

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="img-transparent">
-        <head class="flex flex-row justify-content-between p-3">
+        <head class="flex flex-row justify-content-between p-5">
             <p class="font-bold text-white text-4xl"> </p>
             <h1 class="font-normal text-white text-4xl Roboto">Etrade</h1>
-            <Button class="font-semibold p-2">Call Us</Button>
+            <Button class="font-semibold p-2 justify-content-center text-lg	w-6rem">Call Us</Button>
         </head>
         
-        <section class="flex flex-column text-white px-8 py-8 gap-3 prueba"> 
-            <h1 class="font-normal text-7xl Barlow">Descubre Ofertas Exclusivas Ahora</h1>
-            <p class="Roboto font-normal text-3xl"> Encuentra las mejores promociones en tecnología y más, 
-                solo por tiempo limitado.
-            </p>
-            <Button class="p-3 justify-content-center font-bold max-w-8rem">Obtenlo Ya</Button>
-        </section>
+        <main class="flex flex-column principal-container justify-content-center"> 
+            <div class="pl-8 flex flex-column text-white gap-4 align-items-start container-width">
+                <h1 class="font-ligth text-8xl Barlow title-width">Descubre Ofertas Exclusivas Ahora</h1>
+                <p class="Roboto font-normal text-xl paragraph-width"> Encuentra las mejores promociones en tecnología y más, 
+                    solo por tiempo limitado.
+                </p>
+                <Button class="p-3 justify-content-center font-bold max-w-8rem">Obtenlo Ya</Button>
+            </div>
+        </main>
 
     </div>
 </template>
@@ -29,8 +31,25 @@ export default {
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center;
-        height: 50rem;
+        height: 53rem;
     }
 
+    .title-width{
+        max-width: 800px;
+        line-height: 100px;
+    }
+    
+    .paragraph-width{
+        max-width: 700px;
+        /* padding-left: 10px; */
+    }
+
+    .principal-container{
+        height: 600px;
+    }
+
+    .container-width{
+        width: 1200px;
+    }
 
 </style>
